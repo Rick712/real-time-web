72ee1b544d583be43e2834d80bfd52b2ecffdc97
Rick712 <rick.buter@gmail.com>
2018-04-10 11:59:49 +0200

    removed smelly jquery

--- public/script/script.js
+++ public/script/script.js
@@ -1,15 +1,3 @@
-// $(function () {
-//     var socket = io();
-//     $('form').submit(function () {
-//         socket.emit('chat message', $('#m').val());
-//         $('#m').val('');
-//         return false;
-//     });
-//     socket.on('chat message', function (msg) {
-//         $('#messages').append($('<li>').text(msg));
-//     });
-// });
-
 (function(){
     const socket = io(),
         form = document.querySelector('form'),
