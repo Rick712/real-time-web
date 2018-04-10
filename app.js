35179bd9608a89a23fb9c88f5dc53f9cddf7a7f1
Rick712 <rick.buter@gmail.com>
2018-04-10 15:30:39 +0200

    current number of clients working

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
 const http = require('http').Server(app);
 const io = require('socket.io')(http)
 
 const port = 3000
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+    io.of('/').clients((error, clients) => {
+        if (error) throw error
+        res.render('index', {number: clients.length})
+    })
 })
 
 io.on('connection', function (socket) {
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
+    io.of('/').clients((error, clients) => {
+        if (error) throw error
+        socket.broadcast.emit('leave', { number: clients.length });
+    })
+    socket.on('disconnect', (reason) => {
+        io.of('/').clients((error, clients) => {
+            if (error) throw error
+            console.log( 'currentConnections', clients.length)
+            socket.broadcast.emit('leave', { number: clients.length });
+        })
+    })
 });
+
 
 http.listen(port, function () {
     console.log('Server is online at ' + port)
--- public/script/script.js
+++ public/script/script.js
@@ -3,14 +3,24 @@
         form = document.querySelector('form'),
         m = document.querySelector('input'),
         messages = document.querySelector('#messages')
+
     form.addEventListener('submit', function(e) {
         e.preventDefault()
         socket.emit('chat message', m.value)
-        m.value
+        m.value = ""
     })
+
     socket.on('chat message', function(msg) {
         const listItem = document.createElement('li')
         listItem.innerHTML = msg
         messages.appendChild(listItem)
     })
+
+    socket.on( 'leave', function( msg ) {
+
+        const clients = document.querySelector('.clientCounter')
+        clients.innerHTML = msg.number
+
+    } )
+
 })()
